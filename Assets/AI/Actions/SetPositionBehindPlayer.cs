868c9eef71cd98813c0453125b41673615795f66
Clay Branch <clay@movementstrategy.com>
2013-10-14 14:14:18 -0400

    working on rats moving behind

--- Assets/AI/Actions/SetPositionBehindPlayer.cs
+++ Assets/AI/Actions/SetPositionBehindPlayer.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using RAIN.Core;
 using RAIN.Action;
+using RAIN.Path;
 
 public class SetPositionBehindPlayer : RAIN.Action.Action
 {
@@ -15,9 +16,21 @@ public class SetPositionBehindPlayer : RAIN.Action.Action
     	{
 		GameObject player = agent.actionContext.GetContextItem<GameObject>("player");
 		Vector3 positionBehindPlayer = getPositionBehindPlayer(player);
-		agent.actionContext.SetContextItem<Vector3>("move_target", positionBehindPlayer);
-		agent.actionContext.SetContextItem<bool>("move_target_is_valid", true);
-		return RAIN.Action.Action.ActionResult.SUCCESS;
+		bool positionIsValid = true;
+		RAINPathManager pathManager = agent.PathManager as RAINPathManager;
+		
+		if ((pathManager != null) && (pathManager.gridPathGraph != null)) {
+			Debug.Log (pathManager.gridPathGraph.Quantize(positionBehindPlayer) >= 0);
+			positionIsValid = (pathManager.gridPathGraph.Quantize(positionBehindPlayer) >= 0);
+			Debug.Log (positionIsValid);
+		}
+		
+		if(positionIsValid) {
+			agent.actionContext.SetContextItem<Vector3>("move_target", positionBehindPlayer);
+			return RAIN.Action.Action.ActionResult.SUCCESS;
+		} else {
+			return RAIN.Action.Action.ActionResult.FAILURE;
+		}		
     	}
 	
 	Vector3 getPositionBehindPlayer(GameObject player) {
