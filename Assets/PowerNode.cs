c4c7b910bde9052da0fafcbad2cbf445c7713c22
Clay Branch <clay@movementstrategy.com>
2013-10-17 20:57:50 -0400

    new node behavior is working

--- Assets/PowerNode.cs
+++ Assets/PowerNode.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PowerNode : MonoBehaviour {
 	
@@ -10,13 +11,29 @@ public class PowerNode : MonoBehaviour {
 	bool isDestroyed = false;
 	bool hasPower = false;
 	public bool isPowerSource = false;
-	public PowerNode[] connectedNodes;
+	public PowerNode[] childNodes;
+	List<PowerNode> parentNodes = new List<PowerNode>();
 	public GameObject connectedMachine;
 	
 	// Use this for initialization
 	void Start () {
+		
 		if(isPowerSource) {
-			updateCurrentPowerState(true);
+			addSelfAsParentToChildNodes();
+		}
+	}
+	
+	public void addParentNode(PowerNode parentNode) {
+		parentNodes.Add(parentNode);
+	}
+	
+	public void addSelfAsParentToChildNodes() {
+		foreach(PowerNode powerNode in childNodes) {
+			
+			powerNode.addParentNode(this);
+			powerNode.addSelfAsParentToChildNodes();
+			updateCurrentPowerState();
+			
 		}
 	}
 	// Update is called once per frame
@@ -28,34 +45,47 @@ public class PowerNode : MonoBehaviour {
 		return !isDestroyed;
 	}
 	
-	public void updateCurrentPowerState(bool willBePowered) {
-		
-		if(!isDestroyed) {
-			if(hasPower) {
-				if(!willBePowered) {
-					powerDown();
-				}
-			} else {
-				if(willBePowered) {
-					powerUp();
-				}
+	public bool atLeastOneParentNodeHasPower() {
+		bool atLeastOneHasPower = false;
+		foreach(PowerNode powerNode in parentNodes) {
+			if(powerNode != null && !atLeastOneHasPower) {
+				atLeastOneHasPower = powerNode.isPowered();
 			}
-			updatePowerStateForConnectedNodes();
 		}
+		
+		return atLeastOneHasPower;
+	}
+	
+	public void updateCurrentPowerState() {
+		
+		
+		bool willBePowered = isPowerSource ? true : atLeastOneParentNodeHasPower();
+		willBePowered = isDestroyed ? false : willBePowered; 
+		
+		if(hasPower) {
+			if(!willBePowered) {
+				powerDown();
+			}
+		} else {
+			if(willBePowered) {
+				powerUp();
+			}
+		}
+		updatePowerStateForConnectedNodes();
 	}
 	
 	public void takeDamage(float damageAmount) {
 		integrity -= damageAmount;
 		if(integrity <= 0f) {
-			updateCurrentPowerState(false);
 			isDestroyed = true;
+			updateCurrentPowerState();
 		}
 	}
 	
 	void updatePowerStateForConnectedNodes() {
-		foreach(PowerNode powerNode in connectedNodes) {
+		foreach(PowerNode powerNode in childNodes) {
 			if(powerNode != null) {
-				powerNode.updateCurrentPowerState(hasPower);
+				powerNode.updateCurrentPowerState();
 			}
 		}
 	}
