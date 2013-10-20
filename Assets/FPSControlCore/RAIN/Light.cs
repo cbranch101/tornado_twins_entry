c7460e14ff39090907490055d5bb755cf76fec16
Clay Branch <clay@movementstrategy.com>
2013-10-20 13:10:24 -0400

    test

--- Assets/FPSControlCore/RAIN/Light.cs
+++ Assets/FPSControlCore/RAIN/Light.cs
@@ -9,12 +9,14 @@ public class Light : ShipSystem {
 	RAIN.Path.Dynamic.GraphModifier graphModifier;
 	// Use this for initialization
 	protected override void onStart () {
-
+		
 		graphModifier = (RAIN.Path.Dynamic.GraphModifier) gameObject.GetComponent ("GraphModifier");
 		
 	}
 	
 	protected override void OnPowerDown() {
+		Debug.Log ("powering down");
+		playerHUD.currentMessage = failureNotificaiton;
 		Destroy (gameObject);
 	}
 
