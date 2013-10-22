966a9b91155ddb6c938611697df9c75b7c6ca275
Clay Branch <clay@movementstrategy.com>
2013-10-22 18:40:06 -0400

    added a countdown clock

--- Assets/CoreContainmentSystem.cs
+++ Assets/CoreContainmentSystem.cs
@@ -2,8 +2,18 @@
 using System.Collections;
 
 public class CoreContainmentSystem : ShipSystem {
-
-
+	
+	PlayerHUD playerHUD;
+	
+	protected override void onStart() {
+		GameObject playerHudObject = GameObject.Find ("PlayerHUD");
+		playerHUD = (PlayerHUD) playerHudObject.GetComponent("PlayerHUD");
+	}
+	
+	protected override void triggerPowerDownEffects() {
+		playerHUD.gameStatus = "The whole ship blew up underneath you";
+		playerHUD.endGame();
+	}
 	
 	
 }
