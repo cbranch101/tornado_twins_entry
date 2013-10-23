dddecf3b0d95787e2cfe0bc4b386545b0ca35fd1
Clay Branch <clay@movementstrategy.com>
2013-10-22 22:19:00 -0400

    updated collider for rats

---
+++ Assets/RatHeadCollider.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using FPSControl;
+using RAIN.Core;
+using System.Collections.Generic;
+
+public class RatHeadCollider : MonoBehaviour {
+	
+	public SmallEnemyAI aiComponent;
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void ApplyDamage(DamageSource damageSource) {
+		Debug.Log ("firing");
+		aiComponent.SendMessage("ApplyDamage", damageSource);
+	}
+
+}
