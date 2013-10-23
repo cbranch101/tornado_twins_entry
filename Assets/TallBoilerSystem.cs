a68a29fc7451140bd338402039578bb1ab94b660
Clay Branch <clay@movementstrategy.com>
2013-10-22 23:13:18 -0400

    added lame spray sound

--- Assets/TallBoilerSystem.cs
+++ Assets/TallBoilerSystem.cs
@@ -4,10 +4,14 @@ using System.Collections;
 public class TallBoilerSystem : ShipSystem {
 
 	public ParticleSystem[] steamEffects;
-	
+	public AudioClip steamSound;
+		
 	protected override void triggerPowerDownEffects() {
 		foreach(ParticleSystem steamEffect in steamEffects) {
-			steamEffect.Play();
+			AudioSource audioSource = (AudioSource) gameObject.GetComponent ("AudioSource");
+			audioSource.clip = steamSound;
+			audioSource.Play();
+			steamEffect.Play(); 
 		}
 	} 
 
